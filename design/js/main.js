aa57e480aa74fbfabacbcccd1e9cd398e7c89583
plenipotentSS <stevensonhoyt@icloud.com>
2014-08-11 14:16:36 -0700

    adding preload jquery plugin

--- design/js/main.js
+++ design/js/main.js
@@ -1,9 +1,18 @@
+$.fn.preload = function(f) {
+    this.each(function(){
+        $('<img/>')[0].src = this;
+    });
+    if (typeof f == "function") f();
+}
+
 $(document).ready(function(){
-	$('body').css({
-		'background-image': 'url("../img/empty_road.png")',
-		'background-position': 'center center',
-		'background-size': 'cover',
-		'background-attachment': 'fixed',
-		'background-repeat': 'no-repeat'
-	});
+	$(['../img/empty_road.png']).preload(function() {
+		$('body').css({
+			'background-image': 'url("../img/empty_road.png")',
+			'background-position': 'center center',
+			'background-size': 'cover',
+			'background-attachment': 'fixed',
+			'background-repeat': 'no-repeat'
+		});
+	})
 });
