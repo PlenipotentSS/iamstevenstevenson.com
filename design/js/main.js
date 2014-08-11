3dda03851d1f23320b2268d93b32ebb3506ddf0f
plenipotentSS <stevensonhoyt@icloud.com>
2014-08-11 14:03:18 -0700

    changed size of background image and loaded image after header

--- design/js/main.js
+++ design/js/main.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function(){
+	$('body').css({
+		'background-image': 'url("../img/empty_road.png")',
+		'background-position': 'center center',
+		'background-size': 'cover',
+		'background-attachment': 'fixed',
+		'background-repeat': 'no-repeat'
+	});
+});
