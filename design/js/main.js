6b4589daab38690e3907cb6bfc455da82c8dd87c
plenipotentSS <stevensonhoyt@icloud.com>
2014-08-11 14:26:20 -0700

    attempted to improve preloader

--- design/js/main.js
+++ design/js/main.js
@@ -1,5 +1,6 @@
 $.fn.preload = function(f) {
     this.each(function(){
+    	console.log('loaded:' + this);
         $('<img/>')[0].src = this;
     });
     if (typeof f == "function") f();
@@ -7,6 +8,7 @@ $.fn.preload = function(f) {
 
 $(document).ready(function(){
 	$(['../img/empty_road.png']).preload(function() {
+		console.log('using image...');
 		$('body').css({
 			'background-image': 'url("../img/empty_road.png")',
 			'background-position': 'center center',
