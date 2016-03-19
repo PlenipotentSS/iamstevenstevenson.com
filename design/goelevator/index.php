15029df900a02fede8ec6f6cbae9f73ed87b9b4c
plenipotentSS <stevensonhoyt@icloud.com>
2016-03-19 14:32:53 -0700

    updated for multiple expected repsonse

--- design/goelevator/index.php
+++ design/goelevator/index.php
@@ -38,13 +38,9 @@
     <?php
   } else if (array_key_exists("ip",$params)) {
     $ip = $params["ip"];
-    if (check_alive("http://" . $ip, 100, 301)) { ?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/SVG/DTD/svg10.dtd">
-<svg xmlns="http://www.w3.org/2000/svg" width="99" height="20"><linearGradient id="a" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><rect rx="3" width="99" height="20" fill="#555"/><rect rx="3" x="63" width="36" height="20" fill="#4c1"/><path fill="#4c1" d="M63 0h4v20h-4z"/><rect rx="3" width="99" height="20" fill="url(#a)"/><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="32.5" y="15" fill="#010101" fill-opacity=".3">elevator</text><text x="32.5" y="14">elevator</text><text x="80" y="15" fill="#010101" fill-opacity=".3">up</text><text x="80" y="14">up</text></g></svg>
-    <?php } else { ?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/SVG/DTD/svg10.dtd">
-<svg xmlns="http://www.w3.org/2000/svg" width="99" height="20"><linearGradient id="a" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><rect rx="3" width="99" height="20" fill="#555"/><rect rx="3" x="63" width="36" height="20" fill="#e05d44"/><path fill="#e05d44" d="M63 0h4v20h-4z"/><rect rx="3" width="99" height="20" fill="url(#a)"/><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="32.5" y="15" fill="#010101" fill-opacity=".3">elevator</text><text x="32.5" y="14">elevator</text><text x="80" y="15" fill="#010101" fill-opacity=".3">down</text><text x="80" y="14">down</text></g></svg>
-    <?php }
+    ?>
+<svg height="20" width="198" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><linearGradient id="smooth" x2="0" y2="100%"></linearGradient><stop offset="0" stop-color="#bbb" stop-opacity=".1"></stop><stop offset="1" stop-opacity=".1"></stop><mask id="round"><rect fill="#fff" height="20" rx="3" width="198"></rect></mask><g mask="url(#round)"><rect fill="#555" height="20" width="108"></rect><rect fill="#4c1" height="20" width="90" x="108"></rect><rect fill="url(#smooth)" height="20" width="198"></rect></g><g fill="#fff" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11" text-anchor="middle"><text fill="#010101" fill-opacity=".3" x="54" y="15">app ip</text><text x="54" y="14">current commit</text><text fill="#010101" fill-opacity=".3" x="152" y="15"><?php echo $ip ?></text><text x="152" y="14"><?php echo $ip ?></text></g></svg>
+    <?php
   } else {
     if (check_alive("https://goelevator.com", 100)) { ?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/SVG/DTD/svg10.dtd">
